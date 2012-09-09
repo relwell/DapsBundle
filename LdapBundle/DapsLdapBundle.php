ba52a3134f0a8d2494588261d16f267b59a7cad9
Robert Elwell <robert.elwell@gmail.com>
2012-09-09 15:59:56 -0400

    added security listeners to bundle

--- LdapBundle/DapsLdapBundle.php
+++ LdapBundle/DapsLdapBundle.php
@@ -2,8 +2,22 @@
 
 namespace Daps\LdapBundle;
 
+use Daps\LdapBundle\DependencyInjection\Security\Factory\FormLoginLdapFactory;
+use Daps\LdapBundle\DependencyInjection\Security\Factory\HttpBasicLdapFactory;
+
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class DapsLdapBundle extends Bundle
 {
+    
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new FormLoginLdapFactory());
+        $extension->addSecurityListenerFactory(new HttpBasicLdapFactory());
+    }
+    
 }
