de9b2156d50724ac387dd690e874304a8181b80c
Robert Elwell <robert.elwell@gmail.com>
2012-09-08 22:36:33 -0400

    current progress, still having trouble with configs

--- LdapBundle/DependencyInjection/DapsLdapExtension.php
+++ LdapBundle/DependencyInjection/DapsLdapExtension.php
@@ -22,7 +22,8 @@ class DapsLdapExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('security_services.xml');
+        $loader->load('security_listeners.xml');
     }
 }
