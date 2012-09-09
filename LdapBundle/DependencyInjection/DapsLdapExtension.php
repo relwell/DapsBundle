a6fce6a4bb1a4329e87de27644b1c00a5d0f2c90
Robert Elwell <robert.elwell@gmail.com>
2012-09-09 13:49:05 -0400

    seem to have integrated this right, but now getting some inheritance complaints (probably a good sign)

--- LdapBundle/DependencyInjection/DapsLdapExtension.php
+++ LdapBundle/DependencyInjection/DapsLdapExtension.php
@@ -22,8 +22,7 @@ class DapsLdapExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('security_services.xml');
-        $loader->load('security_listeners.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
